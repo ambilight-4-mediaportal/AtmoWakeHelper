9de51ab029eabfc61ee27f35778ed1bb07ba035e
Rick <RickDB@gmail.com>
2015-02-27 11:31:22 +0100

    Added option to manually specify AtmoWin location, applied automatic code reformatting

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,85 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.btnCloseToTray = new System.Windows.Forms.Button();
-            this.cbComPorts = new System.Windows.Forms.ComboBox();
-            this.atmoWakeNotifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
-            this.btnExitProgram = new System.Windows.Forms.Button();
-            this.cbstartAtmowin = new System.Windows.Forms.CheckBox();
-            this.SuspendLayout();
-            // 
-            // btnCloseToTray
-            // 
-            this.btnCloseToTray.Location = new System.Drawing.Point(160, 12);
-            this.btnCloseToTray.Name = "btnCloseToTray";
-            this.btnCloseToTray.Size = new System.Drawing.Size(167, 38);
-            this.btnCloseToTray.TabIndex = 1;
-            this.btnCloseToTray.Text = "Close to tray";
-            this.btnCloseToTray.UseVisualStyleBackColor = true;
-            this.btnCloseToTray.Click += new System.EventHandler(this.btnCloseToTray_Click);
-            // 
-            // cbComPorts
-            // 
-            this.cbComPorts.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::AtmoWakeHelper.Properties.Settings.Default, "comPort", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-            this.cbComPorts.FormattingEnabled = true;
-            this.cbComPorts.Location = new System.Drawing.Point(13, 12);
-            this.cbComPorts.Name = "cbComPorts";
-            this.cbComPorts.Size = new System.Drawing.Size(120, 21);
-            this.cbComPorts.TabIndex = 0;
-            this.cbComPorts.Text = global::AtmoWakeHelper.Properties.Settings.Default.comPort;
-            this.cbComPorts.TextChanged += new System.EventHandler(this.cbComPorts_TextChanged);
-            // 
-            // atmoWakeNotifyIcon
-            // 
-            this.atmoWakeNotifyIcon.BalloonTipText = "Atmo Wake Helper Service";
-            this.atmoWakeNotifyIcon.BalloonTipTitle = "Atmo Wake Helper Service";
-            this.atmoWakeNotifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("atmoWakeNotifyIcon.Icon")));
-            this.atmoWakeNotifyIcon.Text = "Atmowin Wake Helper";
-            this.atmoWakeNotifyIcon.Visible = true;
-            this.atmoWakeNotifyIcon.Click += new System.EventHandler(this.atmoWakeNotifyIcon_Click);
-            // 
-            // btnExitProgram
-            // 
-            this.btnExitProgram.Location = new System.Drawing.Point(236, 61);
-            this.btnExitProgram.Name = "btnExitProgram";
-            this.btnExitProgram.Size = new System.Drawing.Size(91, 23);
-            this.btnExitProgram.TabIndex = 2;
-            this.btnExitProgram.Text = "Exit program";
-            this.btnExitProgram.UseVisualStyleBackColor = true;
-            this.btnExitProgram.Click += new System.EventHandler(this.btnExitProgram_Click);
-            // 
-            // cbstartAtmowin
-            // 
-            this.cbstartAtmowin.AutoSize = true;
-            this.cbstartAtmowin.Checked = global::AtmoWakeHelper.Properties.Settings.Default.enabledAtmowinStart;
-            this.cbstartAtmowin.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::AtmoWakeHelper.Properties.Settings.Default, "enabledAtmowinStart", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-            this.cbstartAtmowin.Location = new System.Drawing.Point(13, 59);
-            this.cbstartAtmowin.Name = "cbstartAtmowin";
-            this.cbstartAtmowin.Size = new System.Drawing.Size(152, 17);
-            this.cbstartAtmowin.TabIndex = 3;
-            this.cbstartAtmowin.Text = "Start Atmowin after resume";
-            this.cbstartAtmowin.UseVisualStyleBackColor = true;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(335, 91);
-            this.Controls.Add(this.cbstartAtmowin);
-            this.Controls.Add(this.btnExitProgram);
-            this.Controls.Add(this.btnCloseToTray);
-            this.Controls.Add(this.cbComPorts);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "Form1";
-            this.Text = "Atmo Wake Helper Service";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
-            this.Shown += new System.EventHandler(this.Form1_Shown);
-            this.Resize += new System.EventHandler(this.Form1_Resize);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+      this.components = new System.ComponentModel.Container();
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      this.btnCloseToTray = new System.Windows.Forms.Button();
+      this.atmoWakeNotifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+      this.btnExitProgram = new System.Windows.Forms.Button();
+      this.lblAtmoWinFolder = new System.Windows.Forms.Label();
+      this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+      this.btnBrowseAtmoWinFolder = new System.Windows.Forms.Button();
+      this.tbAtmoWinFolder = new System.Windows.Forms.TextBox();
+      this.cbstartAtmowin = new System.Windows.Forms.CheckBox();
+      this.cbComPorts = new System.Windows.Forms.ComboBox();
+      this.SuspendLayout();
+      // 
+      // btnCloseToTray
+      // 
+      this.btnCloseToTray.Location = new System.Drawing.Point(160, 12);
+      this.btnCloseToTray.Name = "btnCloseToTray";
+      this.btnCloseToTray.Size = new System.Drawing.Size(167, 38);
+      this.btnCloseToTray.TabIndex = 1;
+      this.btnCloseToTray.Text = "Close to tray";
+      this.btnCloseToTray.UseVisualStyleBackColor = true;
+      this.btnCloseToTray.Click += new System.EventHandler(this.btnCloseToTray_Click);
+      // 
+      // atmoWakeNotifyIcon
+      // 
+      this.atmoWakeNotifyIcon.BalloonTipText = "Atmo Wake Helper Service";
+      this.atmoWakeNotifyIcon.BalloonTipTitle = "Atmo Wake Helper Service";
+      this.atmoWakeNotifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("atmoWakeNotifyIcon.Icon")));
+      this.atmoWakeNotifyIcon.Text = "Atmowin Wake Helper";
+      this.atmoWakeNotifyIcon.Visible = true;
+      this.atmoWakeNotifyIcon.Click += new System.EventHandler(this.atmoWakeNotifyIcon_Click);
+      // 
+      // btnExitProgram
+      // 
+      this.btnExitProgram.Location = new System.Drawing.Point(236, 137);
+      this.btnExitProgram.Name = "btnExitProgram";
+      this.btnExitProgram.Size = new System.Drawing.Size(91, 23);
+      this.btnExitProgram.TabIndex = 2;
+      this.btnExitProgram.Text = "Exit program";
+      this.btnExitProgram.UseVisualStyleBackColor = true;
+      this.btnExitProgram.Click += new System.EventHandler(this.btnExitProgram_Click);
+      // 
+      // lblAtmoWinFolder
+      // 
+      this.lblAtmoWinFolder.AutoSize = true;
+      this.lblAtmoWinFolder.Location = new System.Drawing.Point(9, 68);
+      this.lblAtmoWinFolder.Name = "lblAtmoWinFolder";
+      this.lblAtmoWinFolder.Size = new System.Drawing.Size(206, 13);
+      this.lblAtmoWinFolder.TabIndex = 5;
+      this.lblAtmoWinFolder.Text = "Manually specify AtmoWin folder (optional)";
+      // 
+      // btnBrowseAtmoWinFolder
+      // 
+      this.btnBrowseAtmoWinFolder.Location = new System.Drawing.Point(251, 91);
+      this.btnBrowseAtmoWinFolder.Name = "btnBrowseAtmoWinFolder";
+      this.btnBrowseAtmoWinFolder.Size = new System.Drawing.Size(75, 23);
+      this.btnBrowseAtmoWinFolder.TabIndex = 6;
+      this.btnBrowseAtmoWinFolder.Text = "Browse";
+      this.btnBrowseAtmoWinFolder.UseVisualStyleBackColor = true;
+      this.btnBrowseAtmoWinFolder.Click += new System.EventHandler(this.btnBrowseAtmoWinFolder_Click);
+      // 
+      // tbAtmoWinFolder
+      // 
+      this.tbAtmoWinFolder.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+      this.tbAtmoWinFolder.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystem;
+      this.tbAtmoWinFolder.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::AtmoWakeHelper.Properties.Settings.Default, "AtmoWinFolder", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+      this.tbAtmoWinFolder.Location = new System.Drawing.Point(12, 91);
+      this.tbAtmoWinFolder.Name = "tbAtmoWinFolder";
+      this.tbAtmoWinFolder.Size = new System.Drawing.Size(232, 20);
+      this.tbAtmoWinFolder.TabIndex = 4;
+      this.tbAtmoWinFolder.Text = global::AtmoWakeHelper.Properties.Settings.Default.AtmoWinFolder;
+      this.tbAtmoWinFolder.Validating += new System.ComponentModel.CancelEventHandler(this.tbAtmoWinFolder_Validating);
+      // 
+      // cbstartAtmowin
+      // 
+      this.cbstartAtmowin.AutoSize = true;
+      this.cbstartAtmowin.Checked = global::AtmoWakeHelper.Properties.Settings.Default.enabledAtmowinStart;
+      this.cbstartAtmowin.CheckState = System.Windows.Forms.CheckState.Checked;
+      this.cbstartAtmowin.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::AtmoWakeHelper.Properties.Settings.Default, "enabledAtmowinStart", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+      this.cbstartAtmowin.Location = new System.Drawing.Point(13, 141);
+      this.cbstartAtmowin.Name = "cbstartAtmowin";
+      this.cbstartAtmowin.Size = new System.Drawing.Size(152, 17);
+      this.cbstartAtmowin.TabIndex = 3;
+      this.cbstartAtmowin.Text = "Start Atmowin after resume";
+      this.cbstartAtmowin.UseVisualStyleBackColor = true;
+      // 
+      // cbComPorts
+      // 
+      this.cbComPorts.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::AtmoWakeHelper.Properties.Settings.Default, "comPort", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+      this.cbComPorts.FormattingEnabled = true;
+      this.cbComPorts.Location = new System.Drawing.Point(13, 12);
+      this.cbComPorts.Name = "cbComPorts";
+      this.cbComPorts.Size = new System.Drawing.Size(120, 21);
+      this.cbComPorts.TabIndex = 0;
+      this.cbComPorts.Text = global::AtmoWakeHelper.Properties.Settings.Default.comPort;
+      this.cbComPorts.TextChanged += new System.EventHandler(this.cbComPorts_TextChanged);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(335, 170);
+      this.Controls.Add(this.btnBrowseAtmoWinFolder);
+      this.Controls.Add(this.lblAtmoWinFolder);
+      this.Controls.Add(this.tbAtmoWinFolder);
+      this.Controls.Add(this.cbstartAtmowin);
+      this.Controls.Add(this.btnExitProgram);
+      this.Controls.Add(this.btnCloseToTray);
+      this.Controls.Add(this.cbComPorts);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.Name = "Form1";
+      this.Text = "Atmo Wake Helper Service";
+      this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+      this.Shown += new System.EventHandler(this.Form1_Shown);
+      this.Resize += new System.EventHandler(this.Form1_Resize);
+      this.ResumeLayout(false);
+      this.PerformLayout();
 
         }
 
@@ -117,6 +156,10 @@
         private System.Windows.Forms.NotifyIcon atmoWakeNotifyIcon;
         private System.Windows.Forms.Button btnExitProgram;
         private System.Windows.Forms.CheckBox cbstartAtmowin;
+        private System.Windows.Forms.TextBox tbAtmoWinFolder;
+        private System.Windows.Forms.Label lblAtmoWinFolder;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnBrowseAtmoWinFolder;
 
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -12,207 +12,239 @@ using System.Windows.Forms;
 
 namespace AtmoWakeHelper
 {
-    public partial class Form1 : Form
+  public partial class Form1 : Form
+  {
+    private static Boolean debugMode = false;
+
+    public Form1()
     {
-        private static Boolean debugMode = false;
+      InitializeComponent();
 
-        public Form1()
+      // Start sleep/wake monitor
+      monitorPowerState();
+
+      if (isProgramRunning("atmowakehelper", 0) > 1)
+      {
+        try
         {
-            InitializeComponent();
-
-            // Start sleep/wake monitor
-            monitorPowerState();
-
-            if (isProgramRunning("atmowakehelper", 0) > 1)
-            {
-                try
-                {
-                    // Close if already running
-                    atmoWakeNotifyIcon.Visible = false;
-                    Environment.Exit(0);
-                }
-                catch { };
-            }
-
-            // No need to lookup COM port count as it only adds failure points
-            int comPortCounter = 1;
-
-            while (comPortCounter <31)
-            {
-                cbComPorts.Items.Add("COM" + comPortCounter);
-                comPortCounter++;
-            }
-
-            // Set com port from user setting
-            if (Properties.Settings.Default.comPort != "")
-            {
-                cbComPorts.Text = Properties.Settings.Default.comPort;
-            }
-            else
-            {
-                cbComPorts.SelectedIndex = 0;
-            }
+          // Close if already running
+          atmoWakeNotifyIcon.Visible = false;
+          Environment.Exit(0);
         }
-
-        public static int isProgramRunning(string name, int runtime)
+        catch
         {
-            foreach (Process clsProcess in Process.GetProcesses())
-            {
-                if (clsProcess.ProcessName.ToLower().Equals(name))
-                {
-                    runtime++;
-                }
-            }
-            return runtime;
         }
-        private void saveSettings()
-        {
-            Properties.Settings.Default.Save();
-        }
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            saveSettings();
-            e.Cancel = true;
-            atmoWakeNotifyIcon.Visible = true;
-            this.Hide();
-        }
+        ;
+      }
 
-        private void cbComPorts_TextChanged(object sender, EventArgs e)
-        {
-            cbComPorts.SelectionLength = 0;
-        }
+      // No need to lookup COM port count as it only adds failure points
+      int comPortCounter = 1;
 
-        private void Form1_Resize(object sender, EventArgs e)
-        {
-            if (FormWindowState.Minimized == this.WindowState)
-            {
-                atmoWakeNotifyIcon.Visible = true;
-                this.Hide();
-            }
+      while (comPortCounter < 31)
+      {
+        cbComPorts.Items.Add("COM" + comPortCounter);
+        comPortCounter++;
+      }
 
-            else if (FormWindowState.Normal == this.WindowState)
-            {
-                if (Properties.Settings.Default.comPort != "")
-                {
-                    atmoWakeNotifyIcon.Visible = true;
-                    this.Hide();
-                }
-                else
-                {
-                    atmoWakeNotifyIcon.Visible = false;
-                }
-            }
-            cbComPorts.SelectionLength = 0;
-        }
-
-        private void atmoWakeNotifyIcon_Click(object sender, EventArgs e)
-        {
-                this.WindowState = FormWindowState.Normal;
-                this.Show();
-        }
-
-        private void btnCloseToTray_Click(object sender, EventArgs e)
-        {
-            atmoWakeNotifyIcon.Visible = true;
-            saveSettings();
-            this.Hide();
-        }
-
-        private void Form1_Shown(object sender, EventArgs e)
-        {
-            if (Properties.Settings.Default.comPort != "")
-            {
-                cbComPorts.SelectionLength = 0;
-                Visible = false;
-                WindowState = FormWindowState.Minimized;
-            }
-        }
-
-        private static void monitorPowerState()
-        {
-            SystemEvents.PowerModeChanged += OnPowerModeChanged;
-        }
-        private static void OnPowerModeChanged(object sender, PowerModeChangedEventArgs e)
-        {
-          switch (e.Mode)
-          {
-            case PowerModes.Resume:
-              string appUSBDeview = @"includes\USBDeview.exe";
-
-              // Sleep 2.5s to allow for disk startup
-              Thread.Sleep(2500);
-
-              if (File.Exists(appUSBDeview))
-              {
-                // Close Atmowin
-                try
-                {
-                  Process[] proc = Process.GetProcessesByName("AtmoWinA");
-                  proc[0].Kill();
-                }
-                catch (Exception eProcAtmoKill)
-                {
-                  logger("Error while closing Atmowin:" + eProcAtmoKill.ToString());
-                }
-
-                // Disconnect COM port
-                logger("Disconnect " + Properties.Settings.Default.comPort);
-                startProcess(appUSBDeview, "/disable_by_drive " + Properties.Settings.Default.comPort);
-
-                // Connect COM port
-                logger("Connect " + Properties.Settings.Default.comPort);
-                startProcess(appUSBDeview, "/enable_by_drive " + Properties.Settings.Default.comPort);
-
-                // Check if we have enabled Atmowin startup to run after resume
-                if (Properties.Settings.Default.enabledAtmowinStart == true)
-                {
-                  // Start Atmowin
-                  logger("Starting atmowin..");
-                  startProcess("AtmoWinA.exe", "");
-                }
-              }
-              else
-              {
-                MessageBox.Show("Missing needed programs, make sure you have copied the folder 'includes' from the downloaded archive");
-              }
-              break;
-          }
-        }
-
-      private static void startProcess(string program, string arguments)
-        {
-            try
-            {
-                ProcessStartInfo startInfo = new ProcessStartInfo();
-                startInfo.FileName = program;
-                startInfo.Arguments = arguments;
-                Process proc = Process.Start(startInfo);
-                proc.WaitForExit(10000);
-            }
-            catch (Exception eStartProcess)
-            {
-              logger(string.Format("Error while starting process ( {0} ) : {1}", program, eStartProcess));
-            }
-
-            // Sleep timer to avoid windows being to quick upon COM port unlocking
-            Thread.Sleep(2500);
-        }
-        private static void logger(string logMessage)
-        {
-            if (debugMode == true)
-            {
-                MessageBox.Show(logMessage);
-            }
-        }
-
-        private void btnExitProgram_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                atmoWakeNotifyIcon.Visible = false;
-                Environment.Exit(0);
-            }
-            catch { };
-        }
+      // Set com port from user setting
+      if (Properties.Settings.Default.comPort != "")
+      {
+        cbComPorts.Text = Properties.Settings.Default.comPort;
+      }
+      else
+      {
+        cbComPorts.SelectedIndex = 0;
+      }
     }
+
+    public static int isProgramRunning(string name, int runtime)
+    {
+      foreach (Process clsProcess in Process.GetProcesses())
+      {
+        if (clsProcess.ProcessName.ToLower().Equals(name))
+        {
+          runtime++;
+        }
+      }
+      return runtime;
+    }
+
+    private void saveSettings()
+    {
+      Properties.Settings.Default.Save();
+    }
+
+    private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+    {
+      saveSettings();
+      e.Cancel = true;
+      atmoWakeNotifyIcon.Visible = true;
+      this.Hide();
+    }
+
+    private void cbComPorts_TextChanged(object sender, EventArgs e)
+    {
+      cbComPorts.SelectionLength = 0;
+    }
+
+    private void Form1_Resize(object sender, EventArgs e)
+    {
+      if (FormWindowState.Minimized == this.WindowState)
+      {
+        atmoWakeNotifyIcon.Visible = true;
+        this.Hide();
+      }
+
+      else if (FormWindowState.Normal == this.WindowState)
+      {
+        if (Properties.Settings.Default.comPort != "")
+        {
+          atmoWakeNotifyIcon.Visible = true;
+          this.Hide();
+        }
+        else
+        {
+          atmoWakeNotifyIcon.Visible = false;
+        }
+      }
+      cbComPorts.SelectionLength = 0;
+    }
+
+    private void atmoWakeNotifyIcon_Click(object sender, EventArgs e)
+    {
+      this.WindowState = FormWindowState.Normal;
+      this.Show();
+    }
+
+    private void btnCloseToTray_Click(object sender, EventArgs e)
+    {
+      atmoWakeNotifyIcon.Visible = true;
+      saveSettings();
+      this.Hide();
+    }
+
+    private void Form1_Shown(object sender, EventArgs e)
+    {
+      if (Properties.Settings.Default.comPort != "")
+      {
+        cbComPorts.SelectionLength = 0;
+        Visible = false;
+        WindowState = FormWindowState.Minimized;
+      }
+    }
+
+    private static void monitorPowerState()
+    {
+      SystemEvents.PowerModeChanged += OnPowerModeChanged;
+    }
+
+    private static void OnPowerModeChanged(object sender, PowerModeChangedEventArgs e)
+    {
+      switch (e.Mode)
+      {
+        case PowerModes.Resume:
+          string appUSBDeview = @"includes\USBDeview.exe";
+          string atmoWinFolder = Properties.Settings.Default.AtmoWinFolder;
+
+          if (!string.IsNullOrEmpty(atmoWinFolder))
+          {
+            appUSBDeview = Path.Combine(atmoWinFolder, @"includes\USBDeview.exe");
+          }
+
+          // Sleep 2.5s to allow for disk startup
+          Thread.Sleep(2500);
+
+          if (File.Exists(appUSBDeview))
+          {
+            // Close Atmowin
+            try
+            {
+              Process[] proc = Process.GetProcessesByName("AtmoWinA");
+              proc[0].Kill();
+            }
+            catch (Exception eProcAtmoKill)
+            {
+              logger("Error while closing Atmowin:" + eProcAtmoKill.ToString());
+            }
+
+            // Disconnect COM port
+            logger("Disconnect " + Properties.Settings.Default.comPort);
+            startProcess(appUSBDeview, "/disable_by_drive " + Properties.Settings.Default.comPort);
+
+            // Connect COM port
+            logger("Connect " + Properties.Settings.Default.comPort);
+            startProcess(appUSBDeview, "/enable_by_drive " + Properties.Settings.Default.comPort);
+
+            // Check if we have enabled Atmowin startup to run after resume
+            if (Properties.Settings.Default.enabledAtmowinStart == true)
+            {
+              // Start Atmowin
+              logger("Starting atmowin..");
+              startProcess("AtmoWinA.exe", "");
+            }
+          }
+          else
+          {
+            MessageBox.Show(
+              "Missing needed programs, make sure you have copied the folder 'includes' from the downloaded archive");
+          }
+          break;
+      }
+    }
+
+    private static void startProcess(string program, string arguments)
+    {
+      try
+      {
+        ProcessStartInfo startInfo = new ProcessStartInfo();
+        startInfo.FileName = program;
+        startInfo.Arguments = arguments;
+        Process proc = Process.Start(startInfo);
+        proc.WaitForExit(10000);
+      }
+      catch (Exception eStartProcess)
+      {
+        logger(string.Format("Error while starting process ( {0} ) : {1}", program, eStartProcess));
+      }
+
+      // Sleep timer to avoid windows being to quick upon COM port unlocking
+      Thread.Sleep(2500);
+    }
+
+    private static void logger(string logMessage)
+    {
+      if (debugMode == true)
+      {
+        MessageBox.Show(logMessage);
+      }
+    }
+
+    private void btnExitProgram_Click(object sender, EventArgs e)
+    {
+      try
+      {
+        atmoWakeNotifyIcon.Visible = false;
+        Environment.Exit(0);
+      }
+      catch
+      {
+      }
+      ;
+    }
+
+    private void btnBrowseAtmoWinFolder_Click(object sender, EventArgs e)
+    {
+      folderBrowserDialog1.ShowDialog();
+      if (!string.IsNullOrEmpty(folderBrowserDialog1.SelectedPath))
+      {
+        tbAtmoWinFolder.Text = folderBrowserDialog1.SelectedPath;
+      }
+      saveSettings();
+    }
+
+    private void tbAtmoWinFolder_Validating(object sender, CancelEventArgs e)
+    {
+      saveSettings();
+    }
+  }
 }
